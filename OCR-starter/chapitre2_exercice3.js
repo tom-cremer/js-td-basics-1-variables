f68d0ac10c82aafaf397508313bee654128cb8a8
tomcremer <tom.cremer@student.hepl.be>
2023-10-11 10:57:03 +0200

    Exercice Finalisé

--- OCR-starter/chapitre2_exercice3.js
+++ OCR-starter/chapitre2_exercice3.js
@@ -1,7 +1,13 @@
 // Permutation de variables
+let a, b, c;
+a = 1;
+b = 2;
 
+c = a;
+a = b;
+b = c;
 
-
+// [a, b] = [b, a] [] = operateur de déconstruction
 
 // Solution classique utilisant une variable temporaire
 
@@ -11,5 +17,4 @@
 /* nombre1 += nombre2;
 nombre2 = nombre1 - nombre2;
 nombre1 -= nombre2; */
-
 
